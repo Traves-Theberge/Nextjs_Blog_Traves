d03b3d6d69e25482c56939c4a8872f6740bf56aa
Traves Theberge <traves.theberge@gmail.com>
2024-08-18 04:29:20 -0400

    Updated

--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -3,14 +3,14 @@
 import React from "react";
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
   const { resolvedTheme } = useTheme();
 
   return (
     <footer className={`py-2 transition-colors duration-300 ease-in-out ${resolvedTheme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-gray-800'}`}>
-      <div className="container mx-auto flex justify-center items-center">
+      <div className="container mx-auto flex justify-center items-center space-x-4">
         <Link 
           href="https://github.com/Traves-Theberge/" 
           target="_blank" 
@@ -18,6 +18,14 @@ export const Footer = () => {
           className={`text-xl ${resolvedTheme === 'dark' ? 'hover:text-gray-300' : 'hover:text-gray-600'}`}
         >
           <FaGithub />
+        </Link>
+        <Link 
+          href="https://www.linkedin.com/in/traves-theberge/" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={`text-xl ${resolvedTheme === 'dark' ? 'hover:text-gray-300' : 'hover:text-gray-600'}`}
+        >
+          <FaLinkedin />
         </Link>
       </div>
     </footer>
